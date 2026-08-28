ff4ec7695155f91fe25fee221ca620c2c9e75ffa
nishizawamotoki <sokomichi.hizawa@gmail.com>
2026-08-28 16:12:19 +0900

    演習問題2を解いた

--- src/components/Exercise2.jsx
+++ src/components/Exercise2.jsx
@@ -1,9 +1,15 @@
+import { devices } from "../data/data";
+
 const Exercise2 = () => {
-  return (
-    <ul>
-      <li>ここに適切なリストを表示してください</li>
-    </ul>
-  );
+  const listDevices = devices
+    .filter((device) => device.width >= 700)
+    .map((device) => (
+      <li key={device.id}>
+        {device.name} {device.price >= 1000 ? "💰 高級品" : "🛒 お買い得"}
+      </li>
+    ));
+
+  return <ul>{listDevices}</ul>;
 };
 
 export default Exercise2;
